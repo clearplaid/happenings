92d7b4c23fbf056357f942bb73f2a883f5f5cf09
Scott <clearplaid@gmail.com>
2019-07-14 22:39:47 -0400

    added necessary files and directories for build, created server.js and commented actions required for routes

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Scraping tools
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// require all models
+var db = require("./models");
+
+var PORT = 3000;
+// Initialize express
+var app = express();
+
+// Configure Middleware
+// use morgan logger for logging requests
+app.use(logger("dev"));
+// parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// make a public static folder
+app.use(express.static("public"));
+
+// connect to Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI)
+
+// Routes
+
+// 1. a GET route to scrape website
+
+// 2. a GET route to retrieve all articles
+
+// 3. a GET route for grabbing a specific article by id, populate with it's comment
+
+// 4. a POST route to save/update an Articles associated comment
+
+
+
+
+// Start the Server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!")
+})
